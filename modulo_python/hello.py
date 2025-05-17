0881062895c30818b067713c8424714f57aefcad
Daniele Simas <dsg.snf20@uea.edu.br>
2025-05-17 10:27:26 -0400

    add: Nova pasta

---
+++ modulo_python/hello.py
@@ -0,0 +1,19 @@
+print("Hello World")
+a = 1
+b =  1.0 
+print(type(a))
+print(type(b))
+
+dici = {"nome": 'Daniele'}
+conj = {'nome','dani'}
+lista = ["nome", "nome"]
+
+
+print(type(conj))
+
+print(type(lista))
+
+
+print(type(dici
+           ))
+           
