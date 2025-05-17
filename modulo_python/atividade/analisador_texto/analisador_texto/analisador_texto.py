155356993104ced65b341317758294101fb27328
Daniele Simas <dsg.snf20@uea.edu.br>
2025-05-17 12:22:39 -0400

    Feat: Feito funções simples

--- modulo_python/atividade/analisador_texto/analisador_texto/analisador_texto.py
+++ modulo_python/atividade/analisador_texto/analisador_texto/analisador_texto.py
@@ -2,8 +2,43 @@
 # Autor: Daniele Simas Guimarães
 
 def analisar_texto(texto):
-    # TODO: implementar análise de texto
-    pass
+
+    textMauisculo = []
+    textMinusculo = []
+    numeroFrequencia = 0
+    texto_curto = ""
+    texto_misto = ""
+    
+    vogais = "aeiouAEIOU"
+
+    palavras = texto.split()
+
+    for caractere in texto:
+        if caractere.isupper():
+            textMauisculo.append(caractere)
+        else:
+            textMinusculo.append(caractere)
+        
+        if caractere.isdigit():
+            texto_misto = "Texto contém dados mistos"
+
+    
+    for caractere in texto:
+        if caractere in vogais:
+            numeroFrequencia+=1
+    
+    if len(palavras) < 10:
+        texto_curto = "Texto muito curto"
+
+    print(texto_curto)
+    print("Texto em maiúsculas", textMauisculo)
+    print("Número de palavras:",len(palavras))
+    print("Número de caracteres (com espaços):",len(texto))
+    print(numeroFrequencia)
+    print(texto_misto)
+   
+    
+
 
 def main():
     continuar = True
